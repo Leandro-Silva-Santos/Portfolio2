19f9ba96ebc693e94a23d68d6ec0f86fcc1dd58a
Leandro <leandro.ls.silva.santos@gmail.com>
2021-07-21 20:22:47 -0300

    removendo a sessão orçamento, ajustando as demais sessões e colocando os links

--- js/script.js
+++ js/script.js
@@ -1,4 +1,27 @@
-document.querySelector(".hamburguer").addEventListener("click", () => 
-  document.querySelector(".container").classList.toggle("show-menu")
-);
+function saibaMais() {
+  window.location.href = "https://leandro-silva-santos.github.io/";
+}
 
+document
+  .querySelector(".hamburguer")
+  .addEventListener("click", () =>
+    document.querySelector(".container").classList.toggle("show-menu")
+  );
+
+function p1() {
+  window.location.href = "https://leandro-silva-santos.github.io/";
+}
+
+function p2() {
+  window.location.href =
+    "https://github.com/Leandro-Silva-Santos/Login-HTML-CSS";
+}
+
+function p3() {
+  window.location.href =
+    "https://github.com/Leandro-Silva-Santos/NetflixCloneFrontEnd";
+}
+
+function mensagem() {
+  
+}
